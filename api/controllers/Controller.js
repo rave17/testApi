8e1ea7c37787e55d3227490ff8bbbb5ea73d7e7a
Roberto Vargas <rave17491@gmail.com>
2020-07-20 02:15:32 -0300

    api files

---
+++ api/controllers/Controller.js
@@ -0,0 +1,50 @@
+const Stud = require('../models/userStud');
+
+module.exports = {
+    getAll: async function (req, res) {
+        const allStudents = await Stud.find();
+        console.log(allStudents);//test
+        res.status(200).json(allStudents);
+    },
+    createStud: async function (req, res) {
+        const stud = new Stud({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        try {
+            const studSaved = await stud.save();
+            console.log(studSaved)//test
+            res.status(200).send(studSaved)
+        } catch (err) {
+            console.log(err + ' data')
+            res.json().status(400).send(err);
+        }
+    },
+    deleteStud: async function (req, res) {
+        try {
+            await Stud.findByIdAndDelete(req.params.id);
+            res.status(200).send('object deleted')
+        } catch (err) {
+            console.log(err)
+            res.status(400).send(err);
+        }
+    },
+    updateStud: async function (req, res) {
+        try {
+            const studEdited = await Stud.findByIdAndUpdate(req.params.id,
+                {
+                    $set: {
+                        name: req.body.name,
+                        lastname: req.body.lastname
+                    }
+                });
+            res.status(200).send('object edited ' + studEdited);
+        } catch (err) {
+            console.log(err)
+            res.status(400).send(err);
+        }
+    }
+}
---
+++ api/models/userStud.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    "name": {type: String, required: true},
+    "lastname": {type: String, required: true},
+    "email": {type: String, required: true},
+    "password": {type: String, required: true}
+});
+
+module.exports = mongoose.model('Students', UserSchema);
