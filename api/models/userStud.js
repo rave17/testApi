3545d18c7bb08acfc1f500998effb403b3e225fa
Roberto Vargas <rave17491@gmail.com>
2020-07-20 23:29:31 -0300

    adding hobbies and data user

--- api/models/userStud.js
+++ api/models/userStud.js
@@ -4,7 +4,11 @@ const UserSchema = mongoose.Schema({
     "name": {type: String, required: true},
     "lastname": {type: String, required: true},
     "email": {type: String, required: true},
-    "password": {type: String, required: true}
+    "password": {type: String, required: true},
+    "hobbies":{type: [String], required: false},
+    "dataUser": {
+        "date": {type: Date, default: Date.now}
+    }
 });
 
 module.exports = mongoose.model('Students', UserSchema);
